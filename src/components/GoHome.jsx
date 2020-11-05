477575ec67c104860f74e5fdf98d706d475570e9
AGasco <antoniofgasco@gmail.com>
2020-11-05 13:11:21 +0000

    transition-between-routes

--- src/components/GoHome.jsx
+++ src/components/GoHome.jsx
@@ -1,16 +1,27 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, withRouter } from "react-router-dom";
 import HomeIcon from "@material-ui/icons/Home";
 import "./../styles/GoHome.css";
 
-function GoHome() {
+const elements = ["/", "/About Me", "/Skills", "/Projects", "/Contact"];
+
+const GoHome = withRouter(({ location, history }) => {
+  const curScreen = elements.indexOf(location.pathname);
   return (
     <div className="goHome">
-      <Link to="/">
+      <div
+        className="goHome__link"
+        onClick={() =>
+          history.push({
+            pathname: "/",
+            state: { prevScreen: curScreen },
+          })
+        }
+      >
         <HomeIcon />
-      </Link>
+      </div>
     </div>
   );
-}
+});
 
 export default GoHome;
