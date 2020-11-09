952346c4208181871c802f9436f9281c8e91d640
AGasco <antoniofgasco@gmail.com>
2020-11-09 10:15:24 +0000

    anims-page-load

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,19 +1,39 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLinkedin, faGithubSquare } from "@fortawesome/free-brands-svg-icons";
 import "./../styles/Landing.css";
 
 const Landing = () => {
+  const [animate, setAnimate] = useState(true);
+
+  useEffect(() => {
+    const hasAnimated = window.sessionStorage.getItem("hasAnimated") !== null;
+    console.log("hasAnimated", hasAnimated);
+    if (hasAnimated) {
+      setAnimate(false);
+    } else {
+      window.sessionStorage.setItem("hasAnimated", 1);
+      setAnimate(true);
+    }
+  }, []);
+
   return (
     <div className="landing" id="Landing">
       <div id="scene" className="landing__container">
         <div data-depth="0.2" className="landing__text">
-          <h1 className="landing__longName">Antonio F. Gasco</h1>
-          <h1 className="landing__shortName">A.F. Gasco</h1>
-          <h4 className="landing__developer">front-end web developer</h4>
+          <h1 className={`landing__longName ${animate && "anim"}`}>
+            Antonio F. Gasco
+          </h1>
+          <h1 className={`landing__shortName ${animate && "anim"}`}>
+            A.F. Gasco
+          </h1>
+          <h4 className={`landing__developer  ${animate && "anim"}`}>
+            front-end web developer
+          </h4>
         </div>
         <div className="landing__icons">
           <a
+            className={`landing__linkedin ${animate && "anim"}`}
             href="https://www.linkedin.com/in/antonio-f-gasco-ojeda-b06b2173"
             target="_blank"
             rel="noopener noreferrer"
@@ -22,6 +42,7 @@ const Landing = () => {
           </a>
 
           <a
+            className={`landing__github ${animate && "anim"}`}
             href="https://github.com/AGasco"
             target="_blank"
             rel="noopener noreferrer"
