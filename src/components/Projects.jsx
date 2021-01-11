9207a8128d2a9884e34c1943033c043325a21e3c
Antonio Gasco <antoniofgasco@gmail.com>
2021-01-11 11:28:25 +0000

    projects-structure

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,6 +6,7 @@ import NetflixThumbnail from "./../imgs/thumbnails/netflix-thumbnail.png";
 import CovidTrackerThumbnail from "./../imgs/thumbnails/covid-tracker-thumbnail.png";
 import HackReactorThumbnail from "./../imgs/thumbnails/hackreactor-thumbnail.PNG";
 import ShoppingCartThumbnail from "./../imgs/thumbnails/shopping-cart-thumbnail.png";
+import KikeMindfulnessThumbnail from "./../imgs/thumbnails/kikemindfulness.JPG";
 import "./../styles/Projects.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUndo } from "@fortawesome/free-solid-svg-icons";
@@ -14,75 +15,97 @@ import Sidebar from "./Sidebar";
 import Hamburger from "./Hamburger";
 import Navbar from "./Navbar";
 
-const projectsData = [
-  //Steam Game
-  // {
-  //   name: "NARCOS PLEASE",
-  //   picture: "https://source.unsplash.com/1600x900",
-  //   description: `I spent the last 3 years of my life designing and building this game. It's a 'Papers Please'-like where you follow the story of a very infamous drug lord from Colombia back in the eighties.
+// const projectsData = [
+//   //Steam Game
+//   // {
+//   //   name: "NARCOS PLEASE",
+//   //   picture: "https://source.unsplash.com/1600x900",
+//   //   description: `I spent the last 3 years of my life designing and building this game. It's a 'Papers Please'-like where you follow the story of a very infamous drug lord from Colombia back in the eighties.
 
-  //     With 30 levels and 15 different endings, this is a fully fledged product. This was the project that made me a programmer.`,
-  //   link: "https://www.youtube.com/watch?v=kxvOABmXsoM", //Trailer link
-  //   gitLink: "", //Steam Page Link
-  //   usedTech:
-  //     "C#, Unity, JSON, Data Structures, Design Patterns, Art (even though it's not tech, I spent countless hours doing art)",
-  // },
+//   //     With 30 levels and 15 different endings, this is a fully fledged product. This was the project that made me a programmer.`,
+//   //   link: "https://www.youtube.com/watch?v=kxvOABmXsoM", //Trailer link
+//   //   gitLink: "", //Steam Page Link
+//   //   usedTech:
+//   //     "C#, Unity, JSON, Data Structures, Design Patterns, Art (even though it's not tech, I spent countless hours doing art)",
+//   // },
 
-  //COMPLETE USER SETTINGS FORM
+//   //COMPLETE USER SETTINGS FORM
+//   {
+//     name: "REDUX USER SETTINGS FORM WITH THEME PICKER",
+//     picture: UserSettingsFormThumbnail,
+//     description:
+//       "A fully fledged user settings form, with different sectors. Each sector may have 2 tabs to differentiate between groups of settings. All state management is done using REDUX",
+//     link: "https://innoloft-agasco-app.web.app/",
+//     gitLink: "https://github.com/AGasco/user-settings-form",
+//     usedTech:
+//       "HTML, CSS, Javascript, React, Redux, Material-UI, JSON, Google Firebase",
+//   },
+
+//   //COVID-19 Tracker
+//   {
+//     name: "REDUX COVID-19 TRACKER",
+//     picture: CovidTrackerThumbnail,
+//     description:
+//       "Keep daily track of COVID-19 incidence all over the world. The data displayed in this app can be sorted worldwide or by country. Furthermore, data can be filtered by Cases, Recovered or Deaths",
+//     link: "https://covid-19-tracker-1b15c.web.app/",
+//     gitLink: "https://github.com/AGasco/covid-19-tracker",
+//     usedTech:
+//       "HTML, CSS, Javascript, React, Redux, JSON, Google Firebase, APIs, Leaflet Maps",
+//   },
+
+//   //REDUX SHOPPING CART
+//   {
+//     name: "REDUX SHOPPING CART",
+//     picture: ShoppingCartThumbnail,
+//     description:
+//       "Fully functional shopping cart app built from scratch. State handling was done using redux and react-redux packages",
+//     link: "https://redux-shopping-cart-fa4e7.firebaseapp.com/",
+//     gitLink: "https://github.com/AGasco/full-shopping-cart",
+//     usedTech: "HTML, CSS, Javascript, React, Redux, Google Firebase, JSON",
+//   },
+
+//   //HACKREACTOR Clone
+//   {
+//     name: "HACKREACTOR CLONE",
+//     picture: HackReactorThumbnail,
+//     description:
+//       "Clone of the HackReactor website. I built this project to train my front-end skills. I wanted to see if I could build a professional looking website. Just 1 html file and 1 css file",
+//     link: "https://hackreactor-clone.netlify.app/",
+//     gitLink: "https://github.com/AGasco/hackreactor-clone",
+//     usedTech: "HTML, CSS, Javascript, Netlify",
+//   },
+
+//   //Netflix Clone
+//   {
+//     name: "NETFLIX CLONE",
+//     picture: NetflixThumbnail,
+//     description:
+//       "Clone of the popular streaming platform, Netflix. It uses Netflix API to obtain movies' data sorted by genres, or to show which movies are trending right now. Each movie can be clicked to watch a Youtube embed showing the trailer of that movie",
+//     link: "https://netflix-clone-adb5c.web.app/",
+//     gitLink: "https://github.com/AGasco/netflix-clone",
+//     usedTech: "HTML, CSS, Javascript, React, Google Firebase, JSON, APIs",
+//   },
+// ];
+
+const commercialProjects = [
   {
-    name: "REDUX USER SETTINGS FORM WITH THEME PICKER",
-    picture: UserSettingsFormThumbnail,
+    name: "Commissioned Website",
+    picture: KikeMindfulnessThumbnail,
     description:
-      "A fully fledged user settings form, with different sectors. Each sector may have 2 tabs to differentiate between groups of settings. All state management is done using REDUX",
-    link: "https://innoloft-agasco-app.web.app/",
-    gitLink: "https://github.com/AGasco/user-settings-form",
-    usedTech:
-      "HTML, CSS, Javascript, React, Redux, Material-UI, JSON, Google Firebase",
+      "Website built for a mindfulness instructor using React. Design, development and communication with client has been fully managed by me. ",
+    link: "https://wikipedia-viewer-4cff5.web.app/",
+    gitLink: "https://github.com/AGasco/kike-mindfulness",
   },
+];
 
-  //COVID-19 Tracker
+const personalProjects = [
   {
-    name: "REDUX COVID-19 TRACKER",
-    picture: CovidTrackerThumbnail,
+    name: "Commissioned Website",
+    picture: KikeMindfulnessThumbnail,
     description:
-      "Keep daily track of COVID-19 incidence all over the world. The data displayed in this app can be sorted worldwide or by country. Furthermore, data can be filtered by Cases, Recovered or Deaths",
-    link: "https://covid-19-tracker-1b15c.web.app/",
-    gitLink: "https://github.com/AGasco/covid-19-tracker",
-    usedTech:
-      "HTML, CSS, Javascript, React, Redux, JSON, Google Firebase, APIs, Leaflet Maps",
-  },
-
-  //REDUX SHOPPING CART
-  {
-    name: "REDUX SHOPPING CART",
-    picture: ShoppingCartThumbnail,
-    description:
-      "Fully functional shopping cart app built from scratch. State handling was done using redux and react-redux packages",
-    link: "https://redux-shopping-cart-fa4e7.firebaseapp.com/",
-    gitLink: "https://github.com/AGasco/full-shopping-cart",
-    usedTech: "HTML, CSS, Javascript, React, Redux, Google Firebase, JSON",
-  },
-
-  //HACKREACTOR Clone
-  {
-    name: "HACKREACTOR CLONE",
-    picture: HackReactorThumbnail,
-    description:
-      "Clone of the HackReactor website. I built this project to train my front-end skills. I wanted to see if I could build a professional looking website. Just 1 html file and 1 css file",
-    link: "https://hackreactor-clone.netlify.app/",
-    gitLink: "https://github.com/AGasco/hackreactor-clone",
-    usedTech: "HTML, CSS, Javascript, Netlify",
-  },
-
-  //Netflix Clone
-  {
-    name: "NETFLIX CLONE",
-    picture: NetflixThumbnail,
-    description:
-      "Clone of the popular streaming platform, Netflix. It uses Netflix API to obtain movies' data sorted by genres, or to show which movies are trending right now. Each movie can be clicked to watch a Youtube embed showing the trailer of that movie",
-    link: "https://netflix-clone-adb5c.web.app/",
-    gitLink: "https://github.com/AGasco/netflix-clone",
-    usedTech: "HTML, CSS, Javascript, React, Google Firebase, JSON, APIs",
+      "Website built for a mindfulness instructor using React. Design, development and communication with client has been fully managed by me. ",
+    link: "https://wikipedia-viewer-4cff5.web.app/",
+    gitLink: "https://github.com/AGasco/kike-mindfulness",
   },
 ];
 
@@ -90,9 +113,13 @@ const categories = ["COMMERCIAL", "PERSONAL"];
 
 function Projects({ isMobile, elements }) {
   const [category, setCategory] = useState("");
+  const [projects, setProjects] = useState([]);
 
   const handleClick = (e) => {
-    setCategory(e.target.textContent);
+    const newCategory = e.target.textContent;
+    setCategory(newCategory);
+    if (newCategory === "COMMERCIAL") setProjects(commercialProjects);
+    else if (newCategory === "PERSONAL") setProjects(personalProjects);
   };
 
   return (
@@ -121,6 +148,19 @@ function Projects({ isMobile, elements }) {
         onClick={() => setCategory("")}
       >
         <FontAwesomeIcon icon={faUndo} />
+      </div>
+      <div className="projects__container">
+        {projects.map((p) => (
+          <Project
+            isMobile={isMobile}
+            name={p.name}
+            picture={p.picture}
+            link={p.link}
+            gitLink={p.gitLink}
+            description={p.description}
+            usedTech={p.usedTech}
+          />
+        ))}
       </div>
 
       {/* {!isMobile ? (
