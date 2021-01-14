282ce4bfe06d46f22099664d054cf7db35a60518
Antonio Gasco <antoniofgasco@gmail.com>
2021-01-14 13:30:14 +0000

    quote-gen-fix

--- src/components/QuoteGenerator.jsx
+++ src/components/QuoteGenerator.jsx
@@ -3,69 +3,68 @@ import * as data from "./../data/quotes.json";
 import "./../styles/QuoteGenerator.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSync } from "@fortawesome/free-solid-svg-icons/";
+import { ContactSupportOutlined } from "@material-ui/icons";
 
 const QuoteGenerator = () => {
   const [curQuote, setCurQuote] = useState("");
-  const [usedQuotes, setUsedQuotes] = useState([]);
+  const [remainingQuotes, setRemainingQuotes] = useState([]);
+
+  useEffect(() => {
+    const { default: quotes } = data;
+    setRemainingQuotes([...quotes]);
+  }, []);
 
   //Initalize with a first quote
   useEffect(() => {
     const { default: quotes } = data;
-    const firstQuote = quotes[Math.floor(Math.random() * quotes.length)];
-    const updatedUsedQuotes = [...usedQuotes];
-    addToUsedQuotes(firstQuote.id);
-    setCurQuote(firstQuote);
-  }, []);
-
-  //Resetting used quotes (whole cycle completed)
-  useEffect(() => {
-    if (usedQuotes.length >= 23) {
-      console.log("resetting quotes");
-      setUsedQuotes([]);
+    if (remainingQuotes) {
+      const firstQuote = remainingQuotes.splice(
+        Math.floor(Math.random() * quotes.length),
+        1
+      )[0];
+      setCurQuote(firstQuote);
     }
-  }, [usedQuotes]);
+  }, [remainingQuotes]);
 
   const generateNewQuote = () => {
-    const { default: quotes } = data;
-    let rng = Math.floor(Math.random() * quotes.length);
+    const newQuote = remainingQuotes.splice(
+      Math.floor(Math.random() * remainingQuotes.length),
+      1
+    )[0];
+    if (remainingQuotes.length === 0) resetQuotes();
 
-    while (usedQuotes.includes(rng)) {
-      console.log(rng + " has already been shown. Trying a new one");
-      rng = Math.floor(Math.random() * quotes.length);
-    }
-    addToUsedQuotes(rng);
-    const newQuote = quotes[rng];
     setCurQuote(newQuote);
   };
 
-  const addToUsedQuotes = (id) => {
+  const resetQuotes = () => {
     const { default: quotes } = data;
-    const updatedUsedQuotes = [...usedQuotes];
-    updatedUsedQuotes.push(quotes[id]);
-    setUsedQuotes(updatedUsedQuotes);
+    setRemainingQuotes([...quotes]);
   };
-
-  // componentDidMount() {
-  //   const { default: quotes } = data;
-  //   const rngQuote = quotes[Math.floor(Math.random() * quotes.length)];
-  //   this.setState({ curQuote: rngQuote });
-  // }
 
   return (
     <div className="quoteGenerator">
       <span className="quoteGenerator__container">
         <div className="quoteGenerator__top">
           <h1>"</h1>
-          <p className="quoteGenerator__quote">{curQuote.quote}</p>
+          <p className="quoteGenerator__quote">{curQuote?.quote}</p>
         </div>
         <div className="quoteGenerator__bottom">
           <button onClick={generateNewQuote} className="quoteGenerator__btn">
             <FontAwesomeIcon icon={faSync} />
           </button>
           <div className="quoteGenerator__author">
-            - {curQuote.author}{" "}
+            -{" "}
+            {
+              <a
+                href={curQuote?.wikipedia}
+                target="_blank"
+                className={curQuote?.author === "Anonymous" && "disabled"}
+              >
+                {curQuote?.author}
+              </a>
+            }{" "}
             <span className="quoteGenerator__date">
-              {curQuote.date ? "(" + curQuote.date + ")" : null}
+              {curQuote?.date ? "(" + curQuote?.date + ")" : null}
             </span>
           </div>
         </div>
